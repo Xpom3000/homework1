74e98f09ca2bce96bc37c1a169215de15abf0c2e
Igor Shipitko <garikrhip20@gmail.com>
2023-10-26 02:06:38 +0300

    Начал делать hw6

---
+++ site/script.js
@@ -0,0 +1,29 @@
+/*
+alert('эзадание 1');
+const massiv = [1, 5, 4, 10, 0, 3];
+for (let i = 0; i <massiv.length; i++) {
+    console.log(massiv[i]);
+    if (massiv[i] === 10) break;      
+    
+}
+
+alert('эзадание 2');
+const massiv1 = [1, 5, 4, 10, 0, 3];
+console.log(`Индекс числа 4 - ${massiv1.indexOf(4)}`);
+
+
+alert('эзадание 3');
+let massiv2 = [1, 3, 5, 10, 20];
+massiv2 = massiv2.join(' ');
+console.log(massiv2);
+*/
+
+alert('эзадание 4');
+
+alert('эзадание 2');
+const massiv1 = [1, 5, 4, 10, 0, 3];
+for (let i = 0; i < massiv1.length; i++) {
+    const element = massiv1[i]===4;
+    
+}
+console.log(`Индекс числа 4 - ${element.index(i)}`);
