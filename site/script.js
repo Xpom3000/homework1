ed8464ea42f495c0db991f61bb1b36fac474ef7d
Igor Shipitko <garikrhip20@gmail.com>
2023-10-27 00:19:19 +0300

    сделал 6 заданий

--- site/script.js
+++ site/script.js
@@ -16,14 +16,35 @@ alert('эзадание 3');
 let massiv2 = [1, 3, 5, 10, 20];
 massiv2 = massiv2.join(' ');
 console.log(massiv2);
-*/
+
 
 alert('эзадание 4');
-
-alert('эзадание 2');
-const massiv1 = [1, 5, 4, 10, 0, 3];
-for (let i = 0; i < massiv1.length; i++) {
-    const element = massiv1[i]===4;
-    
+const result = [];
+for (let i = 0; i < 3; i++) {
+    result[i] = [];
+    for (let j = 0; j < 3; j++) {
+        result[i] [j] = 1;   
+    }
 }
-console.log(`Индекс числа 4 - ${element.index(i)}`);
+console.log(result);
+
+
+alert('эзадание 5');
+let massiv3 = [1, 1, 1];
+massiv3.push(2, 2, 2);
+console.log(massiv3);
+
+
+alert('эзадание 6');
+let arrNumber = [9, 8, 7, 'a', 6, 5];
+arrNumber = arrNumber.filter(i => !isNaN(i)).sort();
+console.log(arrNumber);
+
+const arrNumber1 = [9, 8, 7, 6, 5];
+let num = prompt('Угадай число')
+if (arrNumber1.includes(num)){
+    console.log('Угадал');
+}else{
+    console.log('Не угадал');
+}
+*/
