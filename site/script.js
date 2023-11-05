1a55460c2bf79cdea8eff7e356e1b2ca97cab59f
Igor Shipitko <garikrhip20@gmail.com>
2023-11-06 00:42:57 +0300

    hw7 Начал делать

---
+++ site/script.js
@@ -0,0 +1,25 @@
+
+alert('эзадание 1');
+let reg = 'js';
+reg = reg.toUpperCase();
+console.log(reg);
+
+/*
+alert('Игра №1');
+function seasons(munth) {
+    if (munth >= 3 && munth <= 5) {
+        return "Весна!";
+
+    } else if (munth >= 6 && munth <= 8) {
+        return "Лето";
+        
+    } else if (munth >= 9 && munth <= 11) {
+        return "Осень";
+    
+    } else if (munth == 12 || munth == 1 || munth == 2) {
+        return "Зимма";
+    }    
+    
+}
+alert(seasons(prompt("Введите номер месяца")));
+*/
